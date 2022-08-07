9d80be5c861f65abc6b14eac1500f84330e5342e
nao2033 <naoto-t1217@outlook.jp>
2022-08-07 14:48:18 +0900

    Gセンサー受信部反映_多田

--- ArduinoSide/sensor_control.h
+++ ArduinoSide/sensor_control.h
@@ -1,8 +1,16 @@
+#include <Keyboard.h>
+#include <Wire.h>
 
 #ifndef __SENSOR_CONTROL_H__
 #define __SENSOR_CONTROL_H__
 
 void sensor_setup();
 void sensor_main();
+void sensor_value_send(float,float);
+
+// MPU-6050のアドレス、レジスタ設定値
+#define MPU6050_WHO_AM_I     0x75  // Read Only
+#define MPU6050_PWR_MGMT_1   0x6B  // Read and Write
+#define MPU_ADDRESS  0x68
 
 #endif
