b0d8dc2b2d6ec9c92785943c521f579f6b093e6e
susususuin <kokundntkipwr@gmail.com>
2022-08-27 18:00:38 +0900

    20220827_送信データオフセット処理、チェックサム作成、データ送信処理追加

--- ArduinoSide/serial_control.h
+++ ArduinoSide/serial_control.h
@@ -5,4 +5,17 @@
 void serial_setup();
 void serial_main();
 
+#define 115200 BPS
+#define 1800 OFFSET
+#define 10 TEN_MULT
+#define 8 ONE_BYTE
+enum typedef{
+    ANGLE_X_L;
+    ANGLE_X_H;
+    ANGLE_Y_L;
+    ANGLE_Y_H;
+    CHECKSUM;
+    FRAME_LENGTH;
+}
+
 #endif
